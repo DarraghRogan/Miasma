ccaeb1c1d3b10d6f29c85b54068d32ad1aea5f53
Darragh Rogan <darragh@rogan.ie>
2020-08-21 20:02:28 +0100

    Initial Commit

---
+++ Miasma/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Miasma
+//
+//  Created by Darragh Rogan on 21/08/2020.
+//  Copyright © 2020 Darragh Rogan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Miasma/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Miasma
+//
+//  Created by Darragh Rogan on 21/08/2020.
+//  Copyright © 2020 Darragh Rogan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
