0b1e21e0eade03bd4355ec9fccc009dde6a47293
Darragh Rogan <darraghrogan@kissengineering.ie>
2020-10-01 16:21:37 +0100

    showing WAQI variables in iOS

--- Miasma iOS/ContentView.swift
+++ Miasma iOS/ContentView.swift
@@ -10,35 +10,102 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var stockPrice: Int = 0
-
-       var body: some View {
-
-
-           List {
-
-               HStack {
-                   Text("Stock Price (15 Min. delay)")
-                   Spacer()
-                   Text("\(stockPrice)")
-                       .onAppear() {
-                           self.updateStockPrice(stock: "something")
-                   }
-               }
-
-           }
-
-       }
-
-       private func updateStockPrice(stock: String) {
-
-           DispatchQueue.main.asyncAfter(deadline: .now() + 5) { // sort of URL session task
-               DispatchQueue.main.async { // you need to update it in main thread!
-                self.stockPrice = wAQIData.data?.aqi ?? 0
-               }
-           }
-
-       }
+    
+    @State var ListDestination: String = "0"
+    
+    @State var wAQILink: String = "0"
+    @State var wAQIAttribution: String = "0"
+    @State var wAQICity: String = "0"
+    @State var wAQIAQI: Int = 0
+    @State var wAQIDominentPol: String = "0"
+    @State var wAQITemperature: Double = 0.0
+    @State var wAQITime: String = "0"
+    
+    var body: some View {
+        
+        
+        List
+            {
+            VStack{
+                Text("Air Quality (World Air Quality Index)...")
+                    .fontWeight(.bold).font(.title3)
+                //                Spacer()
+                
+                HStack {
+                    Text("🌍: ")
+                    Spacer()
+                    Text("\(wAQICity)")
+                        .font(.title3)
+                        .onAppear() {
+                            self.updateListEntry()
+                        }
+                }
+                HStack {
+                    Text("📜: ")
+                    Spacer()
+                    Text("\(wAQIAttribution)")
+                        .onAppear() {
+                            self.updateListEntry()
+                        }
+                }
+                HStack {
+                    Text("☁️: ")
+                    Spacer()
+                    Text("\(wAQIAQI) AQI (US EPA PM₂.₅, Current)")
+                        .onAppear() {
+                            self.updateListEntry()
+                        }
+                }
+                HStack {
+                    Text("🎯: Dominant Pollutant: ")
+                    Spacer()
+                    Text("\(wAQIDominentPol)")
+                        .onAppear() {
+                            self.updateListEntry()
+                        }
+                }
+                
+                HStack {
+                    Text("🌡: ")
+                    Spacer()
+                    Text("\(wAQITemperature)")
+                        .onAppear() {
+                            self.updateListEntry()
+                        }
+                }
+                
+                HStack {
+                    Text("📅: Reading Taken: ")
+                    Spacer()
+                    Text("\(wAQITime)")
+                        .onAppear() {
+                            self.updateListEntry()
+                        }
+                }
+                
+            }
+            
+            
+            
+        }
+        
+    }
+    
+    private func updateListEntry() {
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5) { // sort of URL session task
+            DispatchQueue.main.async { // you need to update it in main thread!
+                
+                self.wAQIAttribution = wAQIData.data?.attributions[0].name ?? "0"
+                self.wAQICity = wAQIData.data?.city.name ?? "0"
+                self.wAQIAQI = wAQIData.data?.aqi ?? 0
+                self.wAQIDominentPol = wAQIData.data?.dominentpol ?? "0"
+                self.wAQITemperature = wAQIData.data?.iaqi.t?.v ?? 0
+                self.wAQITime = wAQIData.data?.time.s ?? "0"
+            }
+        }
+        
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
